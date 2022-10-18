52036dab541abb1399e233759f9ece6419889757
Hoang Minh Quang <hoang.minh.quang@classi.jp>
2022-10-18 09:46:54 +0900

    init xcoldec

---
+++ unmarshal.go
@@ -0,0 +1,188 @@
+package xcoldec
+
+import (
+	"encoding"
+	"errors"
+	"fmt"
+	"strconv"
+	"time"
+
+	"reflect"
+
+	"strings"
+
+	"github.com/araddon/dateparse"
+	"github.com/xuri/excelize/v2"
+)
+
+const (
+	defaultTag = "column"
+	sliceDelim = ","
+)
+
+func NewDecoder(row []string) *Decoder {
+	dec := &Decoder{row}
+
+	return dec
+}
+
+type Decoder struct {
+	row []string
+}
+
+func (d *Decoder) Decode(v interface{}) error {
+	vt := reflect.ValueOf(v)
+	if vt.IsNil() || !(vt.Kind() == reflect.Pointer && vt.Elem().Kind() == reflect.Struct) {
+		return errors.New("v must be a pointer of struct")
+	}
+
+	if err := decode(d.row, reflect.ValueOf(v).Elem()); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func decode(row []string, v reflect.Value) error {
+	NumField := v.NumField()
+
+	for i := 0; i < NumField; i++ {
+		sField := v.Type().Field(i)
+		sValue := v.Field(i)
+		// 構造体が埋め込まれていた場合、再帰的に探索
+		if sField.Anonymous {
+			decode(row, sValue)
+			continue
+		}
+
+		col := parseTag(sField.Tag)
+		if col == nil {
+			continue
+		}
+
+		cellVal, err := getCol(row, col.name)
+		if err != nil {
+			return err
+		}
+
+		switch kind := sValue.Kind(); kind {
+		case reflect.Slice:
+			els := strings.Split(cellVal, ",")
+
+			size := len(els)
+			// grow slice size
+			if size >= sValue.Cap() {
+				nv := reflect.MakeSlice(sValue.Type(), sValue.Len(), size)
+				reflect.Copy(nv, sValue)
+				sValue.Set(nv)
+				sValue.SetLen(size)
+			}
+
+			for i, el := range els {
+				if err := decodeScalar(el, sValue.Index(i)); err != nil {
+					sValue.Set(reflect.Zero(sValue.Type()))
+					return err
+				}
+			}
+
+		default:
+			if err := decodeScalar(cellVal, sValue); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+var (
+	textUnmarshaler = reflect.TypeOf((*encoding.TextUnmarshaler)(nil)).Elem()
+)
+
+func decodeScalar(val string, fieldValue reflect.Value) error {
+	if fieldValue.Type().Implements(textUnmarshaler) {
+		fv := fieldValue
+		if fv.IsNil() {
+			fv.Set(reflect.New(fv.Type().Elem()))
+		}
+		if v, ok := fv.Interface().(encoding.TextUnmarshaler); ok {
+			return v.UnmarshalText([]byte(val))
+		}
+	}
+
+	switch kind := fieldValue.Interface(); kind.(type) {
+	case string:
+		fieldValue.SetString(val)
+	case int, int8, int16, int32, int64:
+		n, err := strconv.ParseInt(val, 10, 64)
+		if err != nil {
+			return fmt.Errorf("cannot convert value %q to %T: %w", val, kind, err)
+		}
+		if fieldValue.OverflowInt(n) {
+			return fmt.Errorf("cannot convert value %q: overflow int size", val)
+		}
+		fieldValue.SetInt(n)
+	case uint, uint8, uint16, uint32, uint64:
+		n, err := strconv.ParseUint(val, 10, 64)
+		if err != nil {
+			return fmt.Errorf("cannot convert value %q to %T: %w", val, kind, err)
+		}
+		if fieldValue.OverflowUint(n) {
+			return fmt.Errorf("cannot convert value %q: overflow uint size", val)
+		}
+		fieldValue.SetUint(n)
+	case float32, float64:
+		n, err := strconv.ParseFloat(val, fieldValue.Type().Bits())
+		if err != nil {
+			return fmt.Errorf("cannot convert value %q to %T: %w", val, kind, err)
+		}
+		if fieldValue.OverflowFloat(n) {
+			return fmt.Errorf("cannot convert value %q: overflow float size", val)
+		}
+		fieldValue.SetFloat(n)
+	case bool:
+		switch val {
+		case "true":
+			fieldValue.SetBool(true)
+		case "false":
+			fieldValue.SetBool(false)
+		default:
+			return fmt.Errorf("invalid boolean: %s", val)
+		}
+	case time.Time:
+		n, err := dateparse.ParseAny(val)
+		if err != nil {
+			return fmt.Errorf("cannot convert value %q to %T: %w", val, kind, err)
+		}
+		fieldValue.Set(reflect.ValueOf(n))
+
+	default:
+		return fmt.Errorf("%T is cannot unmarshal", kind)
+	}
+	return nil
+}
+
+type option struct {
+	name string
+}
+
+func parseTag(tag reflect.StructTag) *option {
+	val, ok := tag.Lookup(defaultTag)
+	if !ok {
+		return nil
+	}
+
+	return &option{name: val}
+}
+
+func getCol(row []string, col string) (string, error) {
+	c, err := excelize.ColumnNameToNumber(col)
+	if err != nil {
+		return "", err
+	}
+
+	if len(row) < c {
+		return "", nil
+	}
+	return row[c-1], nil
+}
---
+++ unmarshal_test.go
@@ -0,0 +1,82 @@
+package xcoldec
+
+import (
+	"testing"
+	"time"
+
+	"github.com/araddon/dateparse"
+	"github.com/google/go-cmp/cmp"
+)
+
+type testStruct struct {
+	Str      string    `column:"A"`
+	Int      int       `column:"B"`
+	Uint     uint      `column:"C"`
+	Boolean  bool      `column:"D"`
+	Float32  float32   `column:"E"`
+	Float64  float64   `column:"F"`
+	StrSlice []string  `column:"G"`
+	IntSlice []int     `column:"H"`
+	Time     time.Time `column:"I"`
+}
+
+func TestUnmarshal(t *testing.T) {
+	timeStr := "2021-08-15 02:30:45"
+	timeData, _ := dateparse.ParseAny(timeStr)
+	testCases := []struct {
+		name    string
+		row     []string
+		want    *testStruct
+		wantErr bool
+	}{
+		{
+			"ok with all data type",
+			[]string{"str", "64", "128", "true", "3.14", "6.28", "a,b,c", "1,2,3", "2021-08-15 02:30:45"},
+			&testStruct{Str: "str", Int: 64, Uint: 128, Boolean: true, Float32: 3.14, Float64: 6.28, StrSlice: []string{"a", "b", "c"}, IntSlice: []int{1, 2, 3}, Time: timeData},
+			false,
+		},
+		{
+			"invalid int type",
+			[]string{"str", "invalid"},
+			&testStruct{Str: "str"},
+			true,
+		},
+		{
+			"invalid boolean type",
+			[]string{"str", "64", "128", "invalid"},
+			&testStruct{Str: "str", Int: 64, Uint: 128},
+			true,
+		},
+		{
+			"invalid float type",
+			[]string{"str", "64", "128", "true", "invalid"},
+			&testStruct{Str: "str", Int: 64, Uint: 128, Boolean: true},
+			true,
+		},
+		{
+			"invalid int slice type",
+			[]string{"str", "64", "128", "true", "3.14", "6.28", "a,b,c", "invalid, invalid"},
+			&testStruct{Str: "str", Int: 64, Uint: 128, Boolean: true, Float32: 3.14, Float64: 6.28, StrSlice: []string{"a", "b", "c"}},
+			true,
+		},
+		{
+			"invalid time.Time type",
+			[]string{"str", "64", "128", "true", "3.14", "6.28", "a,b,c", "1,2,3", "invalid"},
+			&testStruct{Str: "str", Int: 64, Uint: 128, Boolean: true, Float32: 3.14, Float64: 6.28, StrSlice: []string{"a", "b", "c"}, IntSlice: []int{1, 2, 3}},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		dec := NewDecoder(tc.row)
+		var got testStruct
+		err := dec.Decode(&got)
+		gotErr := err != nil
+		if gotErr != tc.wantErr {
+			t.Fatalf("wantErr=%v but got=%v", tc.wantErr, gotErr)
+		}
+		if diff := cmp.Diff(tc.want, &got); diff != "" {
+			t.Errorf("-want, +got:\n%s", diff)
+		}
+	}
+}
